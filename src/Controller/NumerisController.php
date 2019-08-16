949bd78352ccc0dbe507ba2f5ba828000bc0aa05
fadel LAGNIKA <adissalagnika23@gmail.com>
2019-08-16 14:45:07 +0100

    Premier commit

---
+++ src/Controller/NumerisController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NumerisController extends AbstractController
+{
+    /**
+     * @Route("/numeris", name="numeris")
+     */
+    public function index()
+    {
+        return $this->render('numeris/index.html.twig', [
+            'controller_name' => 'NumerisController',
+        ]);
+    }
+}
