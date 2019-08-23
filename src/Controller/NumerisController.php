0fb523b7800a60c2ccae24a6bebcbef8f8027b85
fadel LAGNIKA <adissalagnika23@gmail.com>
2019-08-23 12:47:42 +0100

    creation des pages du site

--- src/Controller/NumerisController.php
+++ src/Controller/NumerisController.php
@@ -8,12 +8,41 @@ use Symfony\Component\Routing\Annotation\Route;
 class NumerisController extends AbstractController
 {
     /**
-     * @Route("/", name="numeris")
+     * @Route("/", name="home")
      */
     public function index()
     {
         return $this->render('numeris/index.html.twig', [
             'controller_name' => 'NumerisController',
         ]);
+    }
+
+
+    /**
+     * @Route("/produitservice", name="produitservice")
+     */
+    public function homea() {
+    	return $this->render('numeris/produitservice.html.twig');
+    }
+
+    /**
+     * @Route("/examens", name="examens")
+     */
+    public function homeb() {
+    	return $this->render('numeris/examens.html.twig');
+    }
+
+    /**
+     * @Route("/carriere", name="carriere")
+     */
+    public function homec() {
+    	return $this->render('numeris/carriere.html.twig');
+    }
+
+    /**
+     * @Route("/formations", name="formations")
+     */
+    public function homed() {
+    	return $this->render('numeris/formations.html.twig');
     }
 }
